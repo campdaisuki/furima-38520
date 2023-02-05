82b5ad70eb9a996e2afdbd9d450ab10974508158
campdaisuki <loveactuallyforever@ezweb.ne.jp>
2023-02-05 21:53:37 +0900

    ユーザーの新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :nickname, presence: true, length: {maximum: 40}
+  validates :email, presence: true
+  validates :encrypted_password, presence: true, length: {minimum: 6}
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_day, presence: true
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
 end
