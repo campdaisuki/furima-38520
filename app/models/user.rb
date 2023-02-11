38c7cdcea58bdbf728b6a0142ffffa679ff9f2e9
campdaisuki <loveactuallyforever@ezweb.ne.jp>
2023-02-11 18:09:20 +0900

    テストコードの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :nickname, presence: true, length: {maximum: 40}
-  validates :email, uniqueness: true
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password,  length: {minimum: 6}, format: { with: VALID_PASSWORD_REGEX }, confirmation: true
-  validates :password_confirmation, presence: true
-  validates :family_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :password,  format: { with: VALID_PASSWORD_REGEX }, confirmation: true
+  validates :family_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々]/ }
+  validates :first_name, presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々]/ }
   validates :family_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
   validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
   validates :birth_day, presence: true
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,13 +2,13 @@ FactoryBot.define do
 
   factory :user do
     nickname              {"kumamon"}
-    email                 {"sample@sample.com"}
+    email                 {Faker::Internet.email}
     password              {"1234567"}
-    encrypted_password    {"1234567"}
+    password_confirmation {"1234567"}
     family_name           {"熊本"}
     family_name_kana      {"クマモト"}
     first_name            {"太郎"}
     first_name_kana       {"タロウ"}
     birth_day             {"2000-01-01"}
   end
-end
+end   
