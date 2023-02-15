3ec153b074193fee7e30d39f33335c41dd0cd2ab
campdaisuki <loveactuallyforever@ezweb.ne.jp>
2023-02-15 20:43:43 +0900

    出品する」ボタンを押すと、商品情報がデータベースに保存される

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,13 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   has_one_attached :image
-
+  belongs_to :category
+  belongs_to :shipping_charge
+  belongs_to :condition
+  belongs_to :prefecture
+  belongs_to :days_to_ship
+  
   validates :name, :text, :price, :category_id, :shipping_charge_id,:condition_id, :prefecture_id, :days_to_ship_id, presence: true
-  validates :genre_id, numericality: { other_than: 1 } 
+  #validates :genre_id, numericality: { other_than: 1 } 
   
 end
