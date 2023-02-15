7ccde923f6a3728397ee0a716c4bee5bf79e7b01
campdaisuki <loveactuallyforever@ezweb.ne.jp>
2023-02-15 11:29:17 +0900

    ActiveStorage、ActiveHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  has_one_attached :image
+
+  validates :name, :text, :price, :category_id, :shipping_charge_id,:condition_id, :prefecture_id, :days_to_ship_id, presence: true
+  validates :genre_id, numericality: { other_than: 1 } 
+  
 end
